76b760930df818cf9c51b899c8625770026e6316
Paul Kent <paulandrewkent@hotmail.com>
2025-02-24 14:39:51 +0000

    Add project files.

---
+++ BlazorBuilds.Components/Common/Enums/Enums.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorBuilds.Components.Common.Enums;
+
+public enum StyleAs:     int { Dynamic, OnDark, OnLight };
+public enum RatingScale: int { ThreeStar =3, FiveStar =5, TenStar =10};
+public enum StarSize:    int {Normal, Enhanced};
+
+
---
+++ BlazorBuilds.Components/Common/Seeds/GlobalValues.cs
@@ -0,0 +1,33 @@
+﻿using BlazorBuilds.Components.Common.Enums;
+
+namespace BlazorBuilds.Components.Common.Seeds;
+
+public class GlobalValues
+{
+    public const string Rating_Label_Exception_Message         = "The rating label parameter is required. It cannot be null, empty or whitespace";
+
+    public const string Aria_Rating_Name                       = "Star";
+    public const string Aria_Out_Of_Text                       = "out of";
+
+    public const string StarRating_Class                       = "star-rating";
+    public const string StarRating_Label_Class                 = $"{StarRating_Class}__label";
+    public const string StarRating_Icon_Class                  = $"{StarRating_Class}__icon";
+    public const string StarRating_Button_Class                = $"{StarRating_Class}__button";
+    public const string StarRating_Icon_Solid_Modifier_Class   = $"{StarRating_Icon_Class}--solid";
+    public const string StarRating_Icon_Outline_Modifier_Class = $"{StarRating_Icon_Class}--outline";
+    public const string StarRating_Label_Hidden_Modifier_Class = $"{StarRating_Label_Class}--hidden";
+    public const string StarRating_Large_Target_Modifier_Class = $"{StarRating_Class}--large-target";
+
+    public const string Style_As_Dark  = "dark";
+    public const string Style_As_Light = "light";
+
+    public static string? GetStyleAsValue(StyleAs styleAs)
+
+        => styleAs switch
+        {
+            StyleAs.OnLight => Style_As_Light,
+            StyleAs.OnDark => Style_As_Dark,
+            _ => null
+        };
+
+}
